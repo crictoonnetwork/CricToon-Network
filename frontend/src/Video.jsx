1a262b39616187ee9157202d7d9361a69a7b400e
crictoonnetwork <crictoonnetwork@gmail.com>
2026-03-28 12:20:47 +0530

    Create Video component for editing cricket animation videos.

---
+++ frontend/src/Video.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Video = () => {
+    return (
+        <div>
+            <h1>Edit Cricket Animation Videos</h1>
+            {/* Add your video editing features here */}
+        </div>
+    );
+};
+
+export default Video;
