f190a3f5c035e61f16182be2dab5376a5b5757f3
crictoonnetwork <crictoonnetwork@gmail.com>
2026-03-28 12:19:52 +0530

    Add Animation Library Component

---
+++ frontend/src/Library.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Library = () => {
+    return (
+        <div>
+            <h1>Animation Library</h1>
+            <p>This is a placeholder for the animation library used within CricToon Network.</p>
+        </div>
+    );
+};
+
+export default Library;
