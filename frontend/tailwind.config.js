d986457f3556289ab16c3c9b7129c65b7432ca23
crictoonnetwork <crictoonnetwork@gmail.com>
2026-03-28 12:14:46 +0530

    Create Tailwind CSS configuration file

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        darkBlue: '#0f172a',
+        orange: '#c2410c',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
